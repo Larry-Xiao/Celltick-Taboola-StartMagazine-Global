79863a833e6ec506296bec5b7f8af85bcad3dbc8
LarryXiao <larryx@LarryXiaos-MacBook-Pro.local>
2023-08-12 13:35:31 +0800

    Initial commit by LarrXiao

---
+++ app/src/main/java/com/celltick/apac/news/util/ThemeTool.java
@@ -0,0 +1,18 @@
+package com.celltick.apac.news.util;
+
+import android.app.Activity;
+
+
+/**
+ * 改变主题工具类
+ * 全局变量可以利用 类静态变量 或 preference
+ */
+public class ThemeTool {
+
+
+    public static void changeTheme(Activity activity) {
+        if (PrefUtils.isDarkMode()) {
+//            activity.setTheme(R.style.AppThemeDark);
+        }
+    }
+}
